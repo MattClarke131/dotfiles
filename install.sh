815bbf8525374cc4f2c42b3cbfa225f67cd3a086
matt <mattclarkeruns@gmail.com>
2020-04-18 12:26:11 -0400

    Rewrite install.sh to backup when overwritten

--- install.sh
+++ install.sh
@@ -11,36 +11,35 @@ set -e
 cd "$(dirname "$0")"
 DOTFILES_ROOT=$(pwd -P)
 
+#create subdirectory for current time this script is run
+DOTFILES_OLD_DIR=$DOTFILES_ROOT/dotfiles_old/$(date +%G-%m-%d_%H-%M-%S)
+mkdir -p $DOTFILES_OLD_DIR
 
-# Creates a symlink
-# arg 1: source file
-# arg 2: destination file
-link_file () {
-  local source=$1 destination=$2
 
-  local proceed=true
 
-  # Does the destination file already exist?
+install_dotfile () {
+  local source=$1 destination=$2 backup_destination=$3
+
+  # Create copy of destination in backup_destination
   if test -f "$destination" ; then
-    echo "$destination" "exists. Overwrite? (y/n)"
-    read -r resp
-    if [ "$resp" != 'y' ] && [ "$resp" != 'Y' ] ; then
-      proceed=false
-    fi
+    create_backup_file "$destination" "$backup_destination"
+    echo "existing $destination moved to $backup_destination"
   fi
 
-
-  if [ $proceed = true ] ; then
-    ln -sfv "$source" "$destination"
-  fi
+  # Create symlink at destination pointing to source
+  ln -sfv "$source" "$destination"
 }
 
+create_backup_file () {
+  local source=$1 destination=$2
+  cp $1 $2
+}
 
-#create symlinks for dotfiles
+# Install dotfiles
 echo "Creating symlinks for dot files"
-link_file "$DOTFILES_ROOT"/vimrc "$HOME"/.vimrc
-link_file "$DOTFILES_ROOT"/zshrc "$HOME"/.zshrc
-link_file "$DOTFILES_ROOT"/tmux.conf "$HOME"/.tmux.conf
+install_dotfile "$DOTFILES_ROOT"/vimrc "$HOME"/.vimrc "$DOTFILES_OLD_DIR"/vimrc
+install_dotfile "$DOTFILES_ROOT"/zshrc "$HOME"/.zshrc "$DOTFILES_OLD_DIR"/zshrc
+install_dotfile "$DOTFILES_ROOT"/tmux.conf "$HOME"/.tmux.conf "$DOTFILES_OLD_DIR"/tmux.conf
 echo "symlinks created"
 
 echo "sourcing zshrc"
