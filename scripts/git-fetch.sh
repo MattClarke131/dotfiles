345e28961451b4cafb45be02c12d5e4c319ae125
matt <mattclarkeruns@gmail.com>
2020-08-29 21:21:30 -0400

    [script] Send mail if gtd or dotfile repo needs to be updated

--- scripts/git-fetch.sh
+++ scripts/git-fetch.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-#### Description: Fetches repositories
+#!/usr/bin/env bash
+#### Description: Fetches repositories, echos if repo is behind
+####              If this is run in a cron, it will generate mail
 
 declare -ar REPOSITORIES=(
   "$HOME"/dotfiles
@@ -8,9 +9,15 @@ declare -ar REPOSITORIES=(
 
 for REPO in "${REPOSITORIES[@]}"
 do
-  if [ -d REPO ]
+  if [ -d $REPO ]
   then
     cd "$REPO" || exit
     git fetch
+
+    diff=$(git diff origin/master | wc -c)
+    if [ $diff -ne 0 ]
+    then
+      echo "$REPO has updates!"
+    fi
   fi
 done
