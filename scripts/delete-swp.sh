8e26d83195ac256e7e39b72e4e937442337d3b00
matt <mattclarkeruns@gmail.com>
2020-08-29 21:42:42 -0400

    [script] Add check for vim processes in delete-swp script

--- scripts/delete-swp.sh
+++ scripts/delete-swp.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 #### DESCRIPTION: Finds and deletes all .swp files within current directory (and its children)
 
-find . -name '*.swp' -exec rm {} \;
+num_processes=$(ps -aux | grep vim | wc -l)
+if [ $num_processes -gt 1 ]
+then
+  echo "You have vim sessions running!"
+  echo "(Ignore the grep process)"
+  echo "$(ps -aux | grep vim)"
+  echo ""
+  echo "Proceed anyway? (y/n)"
+  read response
+  if [ $response == 'y' ]
+  then
+    find . -name '*.swp' -exec rm {} \;
+  fi
+else
+  find . -name '*.swp' -exec rm {} \;
+fi
