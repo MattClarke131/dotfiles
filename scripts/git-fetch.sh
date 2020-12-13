3c283223259a6df5d7c1a36db452636fd7a6024e
matt <mattclarkeruns@gmail.com>
2020-12-13 00:31:16 -0500

    [script] Update git-fetch script comment

--- scripts/git-fetch.sh
+++ scripts/git-fetch.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
-#### Description: Fetches repositories, echos if repo is behind
-####              If this is run in a cron, it will generate mail
 
 declare -ar REPOSITORIES=(
   "$HOME"/dotfiles
