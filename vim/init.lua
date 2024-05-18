74a3aa6da38ca5a45a94e5806322738a09157ab0
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-18 02:34:40 +0000

    [VIM] Install typescript language server

--- vim/init.lua
+++ vim/init.lua
@@ -1,5 +1,9 @@
+-- I forget, but I believe this has something to do with neoVim
 vim.cmd([[
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 ]])
+
+-- Language Server Protocol
+require'lspconfig'.tsserver.setup{}
