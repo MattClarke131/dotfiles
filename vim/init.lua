c897298462208834ddd5ebd6a05f9f3d841223c3
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-19 17:15:35 +0000

    [VIM] Install lazy package manager

--- vim/init.lua
+++ vim/init.lua
@@ -12,3 +12,20 @@ require'lspconfig'.tsserver.setup{}
 vim.api.nvim_exec([[
 autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup='Visual', timeout=1000}
 ]], false)
+
+-- lazy package manager
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+})
