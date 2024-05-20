3f5b19b4200a59a6adcd92febbf6ca24dabcc031
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-20 01:54:53 +0000

    [VIM] turn on gitgutter highlighting

--- vim/init.lua
+++ vim/init.lua
@@ -36,7 +36,23 @@ require('lazy').setup({
   'junegunn/fzf.vim',
 
   -- visual changes
-  'airblade/vim-gitgutter',
+  {'airblade/vim-gitgutter',
+    config = function()
+      -- Disable default keybindings
+      vim.g.gitgutter_map_keys = 0
+      -- Enable GitGutter line number highlights
+      vim.cmd('GitGutterLineNrHighlightsEnable')
+      -- Enable GitGutter line highlights
+      vim.cmd('GitGutterLineHighlightsEnable')
+      -- Enable GitGutter signs
+      vim.cmd('GitGutterSignsEnable')
+
+      -- Define symbols for added, modified, and removed lines
+      vim.g.gitgutter_sign_added = '+'
+      vim.g.gitgutter_sign_modified = '~'
+      vim.g.gitgutter_sign_removed = '-'
+    end
+  },
   'ap/vim-buftabline',
   {'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
