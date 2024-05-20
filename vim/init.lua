7cc52a1008dc6fa66791b20b51d8d709f12b2e6a
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-20 01:06:08 +0000

    [VIM] Install lualine plugin

--- vim/init.lua
+++ vim/init.lua
@@ -47,13 +47,51 @@ require('lazy').setup({
   -- to try
   -- 'nvim-telescope/telescope.nvim',
   -- 'tpope/vim-fugitive',
-  -- 'nvim-lualine/lualine.nvim',
   -- 'ibhagwan/fzf-lua'
   --
 })
 
 -- Language Server Protocol
 require'lspconfig'.tsserver.setup{}
+
+require'lualine'.setup{
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'diff'},
+    lualine_c = {'branch',},
+    lualine_x = {'searchcount', 'progress'},
+    lualine_y = {'location', 'filename'},
+    -- find something custom to put here
+    lualine_z = {}
+    --lualine_a = {'mode'},
+    --lualine_b = {'branch'},
+    --lualine_c = {'filename'},
+    --lualine_x = {'encoding', 'fileformat', 'filetype'},
+    --lualine_y = {'progress'},
+    --lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+}
 
 -- Leader
 vim.g.mapleader = ' '
