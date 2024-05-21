79a5701ad3fb81136dd2b45faca2f0c8094a8ea2
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-21 23:50:27 +0000

    [VIM] Set nvim-tree config

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,5 +1,18 @@
 require('nvim-tree').setup {
   disable_netrw = true,
   hijack_netrw = true,
-  view = { side = 'left', },
+  view = {
+    side = 'left',
+    number = true,
+  },
+  renderer = {
+    highlight_modified = 'icon',
+    highlight_git = 'icon',
+    highlight_opened_files = 'icon',
+  },
+  modified = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+  },
 }
