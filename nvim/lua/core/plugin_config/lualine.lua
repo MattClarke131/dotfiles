a1d1d53b0ca56c09882af773b45ac03eab59a199
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-22 16:39:28 +0000

    [VIM] fix formatting

--- nvim/lua/core/plugin_config/lualine.lua
+++ nvim/lua/core/plugin_config/lualine.lua
@@ -7,46 +7,46 @@ local function macro_status()  -- Custom component for macro recording
 end
 
 require('lualine').setup {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = {left = '', right = ''},
-      section_separators = {left = '', right = ''},
-      disabled_filetypes = {},
-      always_divide_middle = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      }
-    },
-    sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'diff'},
-      lualine_c = {
-        'branch',
-        {
-          macro_status,
-          color = { fg = '#ff9e64', gui = 'bold' }, -- Optional: color customization
-        }
-    },
-      lualine_x = {'searchcount', 'progress'},
-      lualine_y = {'location', 'filename'},
-      -- find something custom to put here
-      lualine_z = { 'g:help_tip' },
-      --lualine_a = {'mode'},
-      --lualine_b = {'branch'},
-      --lualine_c = {'filename'},
-      --lualine_x = {'encoding', 'fileformat', 'filetype'},
-      --lualine_y = {'progress'},
-      --lualine_z = {'location'}
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
-      lualine_y = {},
-      lualine_z = {}
-    },
-  }
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+  lualine_a = {'mode'},
+  lualine_b = {'diff'},
+  lualine_c = {
+    'branch',
+    {
+      macro_status,
+      color = { fg = '#ff9e64', gui = 'bold' }, -- Optional: color customization
+    }
+  },
+    lualine_x = {'searchcount', 'progress'},
+    lualine_y = {'location', 'filename'},
+    -- find something custom to put here
+    lualine_z = { 'g:help_tip' },
+    --lualine_a = {'mode'},
+    --lualine_b = {'branch'},
+    --lualine_c = {'filename'},
+    --lualine_x = {'encoding', 'fileformat', 'filetype'},
+    --lualine_y = {'progress'},
+    --lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+}
