fb5b7b32779edfa7fe66a7f495944333285db689
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-05-31 16:11:54 +0000

    fixup! [ZSH] Create MOTD

--- zsh/motd.sh
+++ zsh/motd.sh
@@ -21,7 +21,7 @@ cols=$(tput cols)
 rows=$(tput lines)
 
 v_padding=$(( (rows - lines) / 2 ))
-for ((i=1; i<v_padding; i++)); do echo; done
+for ((i=2; i<v_padding; i++)); do echo; done
 while IFS= read -r line; do
   h_padding=$(( (cols - ${#line}) / 2 ))
   printf "%*s%s\n" $h_padding "" "$line"
