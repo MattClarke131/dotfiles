85c744ffccb1ed6a3d7032f48a4ba6c4c7972459
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2024-09-24 15:22:00 -0400

    [ZSH] Lower reduce vertical padding for motd

--- zsh/motd.sh
+++ zsh/motd.sh
@@ -19,13 +19,13 @@ ascii_art="
 local lines=$(wc -l <<< "$ascii_art")
 local cols=$(tput cols)
 local rows=$(tput lines)
-local v_padding=$(( (rows - lines) / 2 ))
+local v_padding=$(( (rows - lines) / 2 - 1))
 
-for ((i=2; i<v_padding; i++)); do echo; done
+for ((i=1; i<v_padding; i++)); do echo; done
 
 while IFS= read -r line; do
   local h_padding=$(( (cols - ${#line}) / 2 ))
   printf "\e[38;5;88m%*s%s\e[0m\n" $h_padding "" "$line"
 done <<< "$ascii_art"
 
-for ((i=0; i<v_padding; i++)); do echo; done
+for ((i=2; i<v_padding; i++)); do echo; done
